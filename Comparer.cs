bc83f1e21f9d9a386ed4e472b210edef06ca5be4
Роберт Амангильдин <AmangildinRM@stud.kai.ru>
2022-12-08 01:26:26 +0300

    Добавьте файлы проекта.

---
+++ Comparer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Comparer
+    {
+        public double FirstValue { get; private set; }
+
+        public double SecondValue { get; private set; }
+
+        public string Result { get; private set; }
+
+        public Comparer ()
+        {
+            FirstValue = 0;
+            SecondValue = 0;
+            Result = string.Empty;
+        }
+
+        public string GetValues ()
+        {
+            try
+            {
+                Console.WriteLine("Че смотришь? Вводи это чертого число: ");
+                FirstValue = Convert.ToDouble (Console.ReadLine());
+
+                Console.WriteLine("Теперь второе число: ");
+                SecondValue = Convert.ToDouble (Console.ReadLine());
+
+                return "Молодец, хоть здесь справился, долбоеб";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "Число нужно ввести, мудак, ЧИСЛО";
+            }
+        }
+
+        public string CompareValues()
+        {
+            Console.WriteLine(GetValues());
+            if (FirstValue > SecondValue)
+            {
+                return "Первое число больше";
+            }
+            else if (FirstValue < SecondValue)
+            {
+                return "Второе число больше";
+            }
+            else
+            {
+                return "Числа равны";
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+              Comparer comparer= new Comparer();
+              comparer.CompareValues();
+              Console.WriteLine("Success");
+           
+        }
+    }
+}
+
